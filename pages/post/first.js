8ccf0784dfb3da6c9d44871ea338bb90f7c074d8
taehyeon <th05662205@gmail.com>
2021-09-27 14:43:30 +0900

    set up

---
+++ pages/post/first.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Header from "../../components/Header.tsx";
+
+export default function First() {
+  return (
+    <>
+      <Head>
+        <title>first</title>
+      </Head>
+      <Header />
+      <p>hello</p>
+    </>
+  );
+}
