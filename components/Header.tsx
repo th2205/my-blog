5527c8d5e5bddfae55232719bcd3d9fa6c9e3ea7
임태현 <th05662205@gmail.com>
2022-01-03 23:41:15 +0900

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,8 @@
 import Head from "next/head";
 import { AiFillGithub } from "react-icons/ai";
 import Heading from "./Heading";
-import HeaderLayout from "./layouts/HeaderLayout";
-import IconListLayout from "./layouts/IconListLayout";
 import Link from "./Link";
+import style from "./Header.module.scss";
 
 type HeaderProps = {
   id?: string;
@@ -18,16 +17,20 @@ export default function Header({ id }: HeaderProps) {
         <title>{id || title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <HeaderLayout>
-        <Link href="/" underline={false}>
-          <Heading as="h2" weight="s">
-            {title}
-          </Heading>
-        </Link>
-        <IconListLayout>
-          <AiFillGithub size={30} />
-        </IconListLayout>
-      </HeaderLayout>
+      <header className={style["ui-header-layout"]}>
+        <div className={style["ui-header-container"]}>
+          <Link href="/" underline={false}>
+            <Heading as="h2" weight="s">
+              {title}
+            </Heading>
+          </Link>
+          <div className={style["ui-icon-group"]}>
+            <Link href="https://github.com/th2205" targetBlank theme="tertiary">
+              <AiFillGithub size={30} />
+            </Link>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
