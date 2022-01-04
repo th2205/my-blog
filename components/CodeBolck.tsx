43794c77dc391127e80bbc850bea243b837624e4
임태현 <th05662205@gmail.com>
2022-01-04 23:29:30 +0900

    style: codeblock

--- components/CodeBolck.tsx
+++ components/CodeBolck.tsx
@@ -12,7 +12,7 @@ export default function CodeBlock({
   const match = /language-(\w+)/.exec(className || "");
 
   return !inline && match ? (
-    <div style={{ fontFamily: "monospace, monospace" }}>
+    <div style={{ fontFamily: "monospace, monospace", marginBottom: "2rem" }}>
       <SyntaxHighlighter
         style={dracula}
         language={match[1]}
