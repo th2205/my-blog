affd1f7730379f9d640eba12e2679ccd88a66341
임태현 <th05662205@gmail.com>
2022-01-13 11:20:20 +0900

    style: component

--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,2 @@
 export type Theme = "primary" | "secondary" | "tertiary" | "quaternary";
-export type Size = "xxs" | "xs" | "s" | "m" | "l" | "xl" | "xxl";
+export type Size = "xxs" | "xs" | "s" | "m" | "l" | "xl" | "xxl" | "xxxl";
