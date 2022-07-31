3d52602917b6dfeca23c321d8f9acb87a1a6a1d3
taehyeon <th05662205@gmail.com>
2022-07-31 19:46:54 +0900

    feat: add tailwindcss

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,10 +1,7 @@
 import { useComment } from "../hooks/useComment";
-import cn from "classnames";
-import style from "./Comment.module.scss";
 
 export default function Comment() {
   const commentRef = useComment();
-  const classes = cn(style["comment-layout"]);
 
-  return <div ref={commentRef} className={classes}></div>;
+  return <div ref={commentRef} className='w-full mt-10 mb-10'></div>;
 }
