9218c00f592f95cb9499d2d7ab513d602138e724
‘th2205’ <th05662205@gmail.com>
2023-10-03 16:23:53 +0900

    feat: update

---
+++ getPosts.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const POSTS_PATH = "/Users/limtaehyeon/Desktop/Opsidian/brain/dev/";
+
+const getPosts = async () => {
+  const data = await fs.readdirSync(POSTS_PATH);
+
+  console.log(data);
+};
+
+getPosts();
